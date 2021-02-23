c4ef2877d0d563833f44c55a4a71612077a0557d
edymimolam <edymimolam@gmail.com>
2021-02-23 17:59:24 +0300

    add text file

---
+++ text.js
@@ -0,0 +1,8 @@
+//Transaction
+export const APPROVE_TEXT = "Waiting for transaction approve..."
+export const TX_PENDING_TEXT = "Waiting for transaction..."
+export const TX_SUCCESS_TEXT = "Transaction succeeded"
+
+//Errors
+export const TOKENS_INPUT_ERROR_TEXT = "Inappropriate tokens amount"
+export const TX_APPROVE_ERROR = "Error occurs during approval proccess. Try to approve transaction again"
