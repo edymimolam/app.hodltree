a85602144d49da0717180e0f4511b0649d71e934
edymimolam <edymimolam@gmail.com>
2021-02-26 18:08:49 +0300

    add hasSider property to Layout

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -28,7 +28,7 @@ export default function AppLayout({
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Layout>
+      <Layout hasSider={true}>
         <Sider
           theme="light"
           breakpoint="lg"
