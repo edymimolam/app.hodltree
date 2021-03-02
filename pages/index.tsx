30e86c84f55732fc28a4d217ee0953c1b52c46d8
edymimolam <edymimolam@gmail.com>
2021-03-02 20:50:49 +0300

    create ui for first two flash loans blocks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,47 @@
 import AppLayout from "../components/AppLayout";
+import { Row, Col, Card, Button } from "antd";
 
-const IndexPage = () => (
-  <AppLayout title="Flash Loans">
-    <h1>Flash Loans</h1>
-  </AppLayout>
-);
+const data = {
+  totalEarnings: 12234567.34,
+  apy: 25.567,
+};
 
-export default IndexPage;
+export default function FlashLoans() {
+  return (
+    <AppLayout title="Flash Loans">
+      <Row gutter={[16, 16]} align="stretch">
+        <Col xs={24} md={12}>
+          <Card className="fl-earnings">
+            <div className="fl-earnings__left">
+              <h3 className="fl-earnings__small-text">Total Earnings</h3>
+              <span className="fl-earnings__big-text">
+                $ {data.totalEarnings}
+              </span>
+            </div>
+            <div className="fl-earnings__right">
+              <h3 className="fl-earnings__small-text">APY</h3>
+              <span className="fl-earnings__big-text">{data.apy}%</span>
+            </div>
+          </Card>
+        </Col>
+        <Col xs={24} md={12}>
+          <Card className="fl-balance">
+            {/* <h1>Right</h1> */}
+            <div className="fl-balance__left">
+              <h3 className="fl-balance__small-text">My Balance</h3>
+              <span className="fl-balance__big-text">$ 12323423</span>
+            </div>
+            <div className="fl-balance__right">
+              <Button size="large" type="primary">
+                Deposit
+              </Button>
+              <Button size="large" type="primary">
+                Withdraw
+              </Button>
+            </div>
+          </Card>
+        </Col>
+      </Row>
+    </AppLayout>
+  );
+}
